cadc9fb9388dd11bcb8f1ce166111d74802ab004
Ilya Bronner <idbronner@gmail.com>
2026-04-06 06:24:09 +0400

    Fix config

--- tina/config.tsx
+++ tina/config.tsx
@@ -8,11 +8,6 @@ const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 export default defineConfig({
   contentApiUrlOverride: "/api/tina/gql",
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  admin: {
-    auth: {
-      useLocalAuth: isLocal,
-    },
-  },
   build: {
     publicFolder: "public",
     outputFolder: "admin",
