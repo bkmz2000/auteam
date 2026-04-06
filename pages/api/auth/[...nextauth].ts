a871bfa646bff06d5880151066ca8b75fdc0d7ab
Ilya Bronner <idbronner@gmail.com>
2026-04-06 21:09:27 +0400

    Fix auth: use non-null assertion for NEXTAUTH_SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,11 +3,12 @@ import { TinaAuthJSOptions } from "tinacms-authjs"
 import databaseClient from "../../../tina/__generated__/databaseClient"
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true"
+const secret = process.env.NEXTAUTH_SECRET!
 
 export default NextAuth(
   TinaAuthJSOptions({
     databaseClient,
-    secret: process.env.NEXTAUTH_SECRET,
+    secret,
     ...(isLocal ? {} : { providers: [] }),
   })
 )
