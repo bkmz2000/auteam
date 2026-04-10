eace1df830b3f771dfb94de012f5d85fcfa8f7ff
Ilya Bronner <idbronner@gmail.com>
2026-04-10 15:26:59 +0400

    fix(auth): separate auth and Tina backend routes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,16 @@
 import NextAuth from "next-auth"
 import { TinaAuthJSOptions, TinaCredentialsProvider } from "tinacms-authjs"
+
 import databaseClient from "../../../tina/__generated__/databaseClient"
 
-const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true"
 
 const handler = NextAuth(
   TinaAuthJSOptions({
     databaseClient,
-    secret: process.env.NEXTAUTH_SECRET || "fallback-secret-do-not-use-in-prod",
+    secret: process.env.NEXTAUTH_SECRET || "dev-secret",
     providers: isLocal ? [TinaCredentialsProvider({ databaseClient })] : [],
   })
-);
+)
 
-export default handler;
+export default handler
