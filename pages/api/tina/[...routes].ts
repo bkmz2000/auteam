b34e12a9453992fb5a6ce81076b84c61e34ff4f8
Ilya Bronner <idbronner@gmail.com>
2026-04-10 18:50:19 +0400

    fix: remove auth wrapper from Tina backend

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,9 +1,8 @@
-import { TinaNodeBackend, LocalBackendAuthProvider } from "@tinacms/datalayer";
+import { TinaNodeBackend } from "@tinacms/datalayer";
 
 import databaseClient from "../../../tina/__generated__/databaseClient";
 
 const handler = TinaNodeBackend({
-  authProvider: LocalBackendAuthProvider(),
   databaseClient,
 });
 
