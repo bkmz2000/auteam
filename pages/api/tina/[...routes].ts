7ea952787de638af909ce2e06e06de5a131d010e
Ilya Bronner <idbronner@gmail.com>
2026-04-10 18:52:08 +0400

    fix: restore LocalBackendAuthProvider

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,8 +1,9 @@
-import { TinaNodeBackend } from "@tinacms/datalayer";
+import { TinaNodeBackend, LocalBackendAuthProvider } from "@tinacms/datalayer";
 
 import databaseClient from "../../../tina/__generated__/databaseClient";
 
 const handler = TinaNodeBackend({
+  authProvider: LocalBackendAuthProvider(),
   databaseClient,
 });
 
