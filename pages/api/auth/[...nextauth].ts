6e74de53f6b656af936a5f0b794abbf622903100
Ilya Bronner <idbronner@gmail.com>
2026-04-10 15:53:39 +0400

    chore: enable Tina Cloud auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
+import { TinaAuthJSOptions } from "tinacms-authjs"
+import databaseClient from "../../../tina/__generated__/databaseClient"
 
-export default NextAuth({
-  secret: process.env.NEXTAUTH_SECRET || "fallback-secret",
-  providers: [],
-})
+export default NextAuth(
+  TinaAuthJSOptions({
+    databaseClient,
+    secret: process.env.NEXTAUTH_SECRET || "fallback-secret",
+  })
+)
